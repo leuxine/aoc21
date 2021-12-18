2c693968ab11ba37146ca70d849a15adb5273d57
Max Kraehenmann <maxkr@student.ethz.ch>
2021-12-19 00:26:53 +0100

    day 12 initial commit

---
+++ day12/main.py
@@ -0,0 +1,9 @@
+
+
+def star1():
+    graph = dict()
+    with open('input.in', 'r') as file:
+        for line in file:
+            edge = line.strip('\n').split('-')
+            graph[edge[0]].dfjalsdf
+            graph[]
