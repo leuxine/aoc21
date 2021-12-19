a9fd99b7369d1239f0ee9dd9ef4eb25179850326
Max Kraehenmann <maxkr@student.ethz.ch>
2021-12-19 19:27:34 +0100

    day14 done

--- day14/main.py
+++ day14/main.py
@@ -2,9 +2,8 @@
 
 def star1():
     rules = dict()
-    pairs = dict()
     polymer = ""
-    with open('small.in', 'r') as file:
+    with open('input.in', 'r') as file:
         polymer = file.readline().strip('\n')
         for line in file:
             if line == '\n':
@@ -12,13 +11,6 @@ def star1():
             tmp = line.strip('\n').split(' ')
             rules[tmp[0]] = tmp[2]
     
-    for i in range(1, len(polymer)):
-        try:
-            pairs[polymer[i-1:i+1]] += 1
-        except:
-            pairs[polymer[i-1:i+1]] = 1
-  
-
     elems = dict()
     for el in polymer:
         try:
@@ -26,37 +18,92 @@ def star1():
         except:
             elems[el] = 1
     
+
     for i in range(10):
+        l = 2 * len(polymer) - 2
+        for j in range(0, l, 2):
+            hd = polymer[:j+1]
+            tl = polymer[j+1:]
+            curr = polymer[j:j+2]
+            polymer = hd + rules[curr] + tl
+            try:
+                elems[rules[curr]] += 1
+            except:
+                elems[rules[curr]] = 1
+
+
+    max_ = -1
+    min_ = 20000000000000
+    for el in elems:
+        if elems[el] > max_:
+            max_ = elems[el]
+        if elems[el] < min_:
+            min_ = elems[el]
+    
+    print("the difference is %d\n" % (max_ - min_))
+
+
+def star2():
+    rules = dict()
+    pairs = dict()
+    polymer = ""
+    with open('input.in', 'r') as file:
+        polymer = file.readline().strip('\n')
+        for line in file:
+            if line == '\n':
+                continue
+            tmp = line.strip('\n').split(' ')
+            rules[tmp[0]] = tmp[2]
+
+    for i in range(1, len(polymer)):
+        try:
+            pairs[polymer[i-1:i+1]] += 1
+        except:
+            pairs[polymer[i-1:i+1]] = 1
+
+
+    elems = dict()
+    for el in polymer:
+        try:
+            elems[el] += 1
+        except:
+            elems[el] = 1
+
+    # everything fine until here
+
+    for i in range(40):
         tmp = pairs.copy()
         for k in pairs:
             if pairs[k] == 0:
                 continue
-            tmp[k] -= 1
+            tmp[k] -= pairs[k]
             fst = k[0]
             trd = k[1]
             snd = rules[k]
             try:
-                tmp[fst+snd] += 1
+                elems[snd] += pairs[k]
             except:
-                tmp[fst+snd] = 1
+                elems[snd] = pairs[k]
             try:
-                tmp[snd+trd] += 1
+                tmp[fst+snd] += pairs[k]
             except:
-                tmp[snd+trd] = 1
+                tmp[fst+snd] = pairs[k]
+            try:
+                tmp[snd+trd] += pairs[k]
+            except:
+                tmp[snd+trd] = pairs[k]
         pairs = tmp
-        print(pairs)
-        print()
-
-    max_ = -1
-    min_ = 9223372036854775807
-    for k in pairs:
-        if pairs[k] > max_:
-            max_ = pairs[k]
-        if pairs[k] < min_:
-            min_ = pairs[k]
-
-    print("the difference is %d\n" % (max_ - min_))
-
+   
     
+    max_ = -1
+    min_ = 20000000000000000
+    for el in elems:
+        if elems[el] > max_:
+            max_ = elems[el]
+        if elems[el] < min_:
+            min_ = elems[el]
+
+    print("the difference for the second star is %d\n" % (max_ - min_))
 
 star1()
+star2()
